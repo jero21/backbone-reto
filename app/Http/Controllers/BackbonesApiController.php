ad9edb1ca6e70d56f3aef70e1578765e20a2cdd0
JeremiasMora <jeremias.morac@gmail.com>
2022-08-14 02:08:19 -0300

    correction in words with accents

--- app/Http/Controllers/BackbonesApiController.php
+++ app/Http/Controllers/BackbonesApiController.php
@@ -16,35 +16,66 @@ class BackbonesApiController extends Controller
      */
     public function show($zip_code)
     {
-        try {
-           
+        try { 
             $zipArray = Zip::where('d_codigo', $zip_code)->get();
             $zipFirst = Zip::where('d_codigo', $zip_code)->first();
             $settlements = [];
             foreach ($zipArray as $key => $value) {
                 $settlements[$key]['key'] = intval($value->id_asenta_cpcons);
-                $settlements[$key]['name'] = strtoupper($value->d_asenta);
+                $settlements[$key]['name'] = strtoupper($this->deleteAccents($value->d_asenta));
                 $settlements[$key]['zone_type'] = strtoupper($value->d_zona);
-                $settlements[$key]['settlement_type']['name'] = $value->d_tipo_asenta;                
+                $settlements[$key]['settlement_type']['name'] = $this->deleteAccents($value->d_tipo_asenta);
             }
             
             return response()->json([
                 'zip_code' => $zipFirst->d_codigo,
-                'locality' => $zipFirst->d_ciudad ? strtoupper($zipFirst->d_ciudad) : "",
+                'locality' => $zipFirst->d_ciudad ? strtoupper($this->deleteAccents($zipFirst->d_ciudad)) : "",
                 'federal_entity' => [
                     'key' => intval($zipFirst->c_estado),
-                    'name' => strtoupper($zipFirst->d_estado),
+                    'name' => strtoupper($this->deleteAccents($zipFirst->d_estado)),
                     'code' => null
                 ],
                 'settlements' => $settlements,
                 'municipality' => [
                     "key" => intval($zipFirst->c_mnpio),
-                    "name" => strtoupper($zipFirst->D_mnpio)
+                    "name" => strtoupper($this->deleteAccents($zipFirst->D_mnpio))
                 ]
             ]);
             
         } catch (ModelNotFoundException $exception) {
             return back()->withError($exception->getMessage())->withInput();
         }
+    }
+
+    function deleteAccents($cadena){
+
+        // we replace letters
+        $cadena = str_replace(
+            array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+            array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+            $cadena
+        );
+    
+        $cadena = str_replace(
+            array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+            array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+            $cadena );
+    
+        $cadena = str_replace(
+            array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+            array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+            $cadena );
+    
+        $cadena = str_replace(
+            array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+            array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+            $cadena );
+    
+        $cadena = str_replace(
+            array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+            array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+            $cadena );
+    
+        return $cadena;
     }
 }
